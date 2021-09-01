d521a47bec38e057a27798e0eb73171f7f7eab2f
Fernando Gomes <fernando.gomes@cnj.jus.br>
2021-08-31 22:49:27 -0300

    implementando menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Suitability', url: '/suitability', icon: 'business' },
+    { title: 'Clientes', url: '/folder/Outbox', icon: 'person' },
+    { title: 'Sair', url: '/folder/Spam', icon: 'power' },
   ];
 
   constructor() {}
