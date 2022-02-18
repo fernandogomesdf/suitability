5a3bb1101ae4a4de41d0a80abfa1658f9bc7a046
Fernando Gomes <fernando.gomes@cnj.jus.br>
2022-02-18 15:30:31 -0300

    criacao dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,10 +1,12 @@
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 import { HeaderModule } from './../header/header.module';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
-
+import { ChartModule } from 'primeng/chart';
 import { DashboardPageRoutingModule } from './dashboard-routing.module';
 
 import { DashboardPage } from './dashboard.page';
@@ -15,7 +17,10 @@ import { DashboardPage } from './dashboard.page';
     FormsModule,
     IonicModule,
     DashboardPageRoutingModule,
-    HeaderModule
+    HeaderModule,
+    CardModule,
+    ChartModule,
+    ButtonModule
   ],
   declarations: [DashboardPage],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPage implements OnInit {
 
+  data: any;
+
+  chartOptions: any;
+
+  subscription: Subscription;
+
   constructor() { }
 
   ngOnInit() {
+    this.data = {
+      labels: ['Iniciantes', 'Intermediário', 'Avançados'],
+      datasets: [
+        {
+          data: [300, 50, 100],
+          backgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56'
+          ],
+          hoverBackgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56'
+          ]
+        }
+      ]
+    };
   }
 
 }
